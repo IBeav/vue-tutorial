de96f39892f24d3b7460d67beb4f16871e0a2141
Lee Keeling <leekeeling@hotmail.co.uk>
2022-04-01 17:17:30 +0100

    followed tutorial more

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,6 +1,6 @@
 <template>
     <div :key="task.id" v-for="task in tasks">
-        <Task v-on:delete-task="$emit('delete-task', task.id)" 
+        <Task @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" 
         :task="task" />
     </div>
 </template>
@@ -16,6 +16,6 @@ export default {
     components: {
         Task,
     },
-    emits: ['delete-tasks'],
+    emits: ['delete-tasks', 'toggle-reminder'],
 }
 </script>
